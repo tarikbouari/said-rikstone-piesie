958c0f9b878d796029e26c896e8706a406f736c7
Said-laasri <saidlaasri3@gmail.com>
2022-04-27 14:54:58 +0000

    first

---
+++ javas.js
@@ -0,0 +1,10 @@
+
+  function code(){
+    document.getElementById("good").style.color="red";
+    document.getElementById("demo").innerHTML = "Boom";
+    document.getElementById("wel").innerHTML = "Good bay!";  
+    document.getElementById("pinguin").src = "duckling-plants-water-pond-ugly-animal-8465-resized.jpg"; 
+    
+}
+document.getElementById("sum").innerHTML = "Good day of coding!";
+
